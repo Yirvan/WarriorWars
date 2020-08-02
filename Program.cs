e52b3e14c40b782a9f5c2af4a7151145f20dc70a
ir4van <ir4van@gmail.com>
2020-08-02 07:37:33 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace WarriorWars
+{
+    class Program
+    {
+        static Random ran = new Random();
+        static void Main()
+        {
+            Warrior goodGuy = new Warrior("Bob", Faction.GoodGuy);
+            Warrior badGuy = new Warrior("Joe", Faction.BadGuy);
+
+            while (goodGuy.IsAlive && badGuy.IsAlive)
+            {
+                if (ran.Next(0,10)<5)
+                {
+                    goodGuy.Attack(badGuy);
+                }
+                else
+                {
+                    badGuy.Attack(goodGuy);
+                }
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
